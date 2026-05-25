348385914de04bb711280cd714c99eaeae2cab0e
haris <261246119+haris-b-dev@users.noreply.github.com>
2026-05-25 18:43:31 +0200

    feat: add create service request infrastructure setup

---
+++ src/Octo.ServicePortal.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Octo.ServicePortal.Application.ServiceRequests;
+
+namespace Octo.ServicePortal.Application
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddApplication(this IServiceCollection services)
+		{
+			services.AddScoped<CreateServiceRequestService>();
+
+			return services;
+		}
+	}
+}
