e7a4a68d6146b4a17e7dce1ef31ca09f7e7073c4
haris <261246119+haris-b-dev@users.noreply.github.com>
2026-05-25 17:45:51 +0200

    feat: add init efconfig

---
+++ src/Octo.ServicePortal.Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Octo.ServicePortal.Domain.Customers;
+using Octo.ServicePortal.Domain.ServiceRequests;
+using Octo.ServicePortal.Domain.Machines;
+
+namespace Octo.ServicePortal.Infrastructure.Persistance
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{			
+		}
+
+		public DbSet<CompanyCustomer> CompanyCustomers => Set<CompanyCustomer>();
+		public DbSet<Machine> Machines => Set<Machine>();
+		public DbSet<ServiceRequest> ServiceRequests => Set<ServiceRequest>();
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+		}
+	}
+}
---
+++ src/Octo.ServicePortal.Infrastructure/Persistance/Configurations/MachineConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Octo.ServicePortal.Domain.Machines;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Octo.ServicePortal.Infrastructure.Persistance.Configurations
+{
+	public class MachineConfiguration : IEntityTypeConfiguration<Machine>
+	{
+		public void Configure(EntityTypeBuilder<Machine> builder)
+		{
+			builder.HasKey(machine => machine.Id);
+			builder.Property(machine => machine.ModelName).IsRequired().HasMaxLength(300);
+			builder.Property(machine => machine.SerialNumber).IsRequired().HasMaxLength(100);
+			builder.Property(machine => machine.CreatedAt).IsRequired();
+
+			builder.HasOne(machine => machine.CompanyCustomer)
+				   .WithMany(companyCustomer => companyCustomer.Machines)
+				   .HasForeignKey(machine => machine.CompanyCustomerId)
+				   .OnDelete(DeleteBehavior.Restrict);
+		}
+	}
+}
